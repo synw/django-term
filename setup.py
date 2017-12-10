243878f610a64624796084c7c0a0c2631a30d2a0
Win <synwe@yahoo.com>
2017-12-10 13:07:48 +0000

    Prepare pip release

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+version = __import__('terminal').__version__
+
+setup(
+    name='django-terminal',
+    packages=find_packages(),
+    include_package_data=True,
+    version=version,
+    description='In browser terminal for Django ',
+    author='synw',
+    author_email='synwe@yahoo.com',
+    url='https://github.com/synw/django-terminal',
+    download_url='https://github.com/synw/django-terminal/releases/tag/' + version,
+    keywords=['django', 'terminal'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django :: 1.11',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=[
+        'django-instant>=0.6',
+    ],
+    zip_safe=False
+)
--- terminal/scripts/testscript.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.models import User
-from terminal.commands import rprint
-
-
-def run():
-    for user in User.objects.all():
-        rprint(user.username)
